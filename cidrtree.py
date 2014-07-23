ffa23da471b50db64bd44327f94d11ad0c4377e2
James Hodgkinson <yaleman@ricetek.net>
2014-07-23 21:27:27 +1000

    starting to define the CIDR stuff

--- cidrtree.py
+++ cidrtree.py
@@ -3,6 +3,8 @@
 """ takes some flat files with host group definitions and does things. this description will get better!
 """
 
+from re import compile
+
 def getfile( filename ):
 	""" gets the contents of a file """
 	return False
@@ -14,3 +16,14 @@ def csvprocess( csvstring ):
 def cleanfile( filestring ):
 	""" cleans not-allowed details, may return a report """
 	return False
+
+CIDRVALIDATE = re.compile( "^[\d]{1,3}.[\d]{1,3}.[\d]{1,3}.[\d]{1,3}/[\d]{1,2}$" )
+
+class CIDR( object ):
+	""" CIDR definition """
+
+	def init( self, CIDRstring ):
+		""" feed it a CIDR x.x.x.x/y and it'll do things. """
+		if not CIDRVALIDATE.match( CIDRstring ):
+			
+			return False
