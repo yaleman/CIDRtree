793952fba35438cccbaaa023cc27555c9e653273
James Hodgkinson <yaleman@ricetek.net>
2014-07-23 19:44:08 +1000

    initial commit

---
+++ cidrtree.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+""" takes some flat files with host group definitions and does things. this description will get better!
+"""
+
+def getfile( filename ):
+	""" gets the contents of a file """
+	return False
+
+def csvprocess( csvstring ):
+	""" deals with a csv string """
+	return False
+
+def cleanfile( filestring ):
+	""" cleans not-allowed details, may return a report """
+	return False
